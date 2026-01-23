699211763488f1f0afddfa01c10cf430e7ab65c6
vmadmin <harrymoore67212@proton.me>
2026-01-23 05:59:47 +0000

    Remove layer lock + some branding features

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -133,7 +133,7 @@ systemctl enable firewalld
 sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
-sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
+#sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
 
 systemctl enable bootc-fetch-apply-updates
 
